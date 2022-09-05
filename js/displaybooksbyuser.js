d4058c7dfa541b68461a9c81e37f72aafbb6f725
Sohil Khanduja <77602316+sohilkhanduja@users.noreply.github.com>
2022-09-05 13:37:11 +0530

    Update displaybooksbyuser.js

--- js/displaybooksbyuser.js
+++ js/displaybooksbyuser.js
@@ -12,44 +12,21 @@ function  getAllBooks() {
         var counter = 0;
         for (var i = 0; i < tableData.length; i++) {
             if(tableData[i].username === username ) {
-            counter +=1;
-            var row = table.insertRow(counter);
-            var cell1 = row.insertCell(0);
-            var cell2 = row.insertCell(1);
-            var cell3 = row.insertCell(2);
-            var cell4 = row.insertCell(3);
-            var cell5 = row.insertCell(4);
-            var cell6 = row.insertCell(5);
-            cell1.innerHTML = counter;
-            cell2.innerHTML = tableData[i].username;
-            cell3.innerHTML = tableData[i].isbn;
-            cell4.innerHTML = tableData[i].title;
-            cell5.innerHTML = tableData[i].author;
-            var date1 = tableData[i].date;
-            var rev = date1.length - 1;
-            for(var k = 0; i < date1.length/2; k++)
-            {
-                var temp = date1[k];
-                date1[k] = date1[rev];
-                date1[rev] = temp;
-                rev--;
-            }
-            cell5.innerHTML = date1;
-            var date2 = tableData[i].returnDate;
-            rev = date2.length - 1;
-            for(var k = 0; i < date2.length/2; k++)
-            {
-                var temp = date2[k];
-                date2[k] = date2[rev];
-                date2[rev] = temp;
-                rev--;
-            }
-            cell6.innerHTML = tableData[i].dateReturned;
-            // var ce11 1 is checkbox
-            cell1.innerHTML = "<input type='checkbox' id='checkbox'>";
-            cell4.innerHTML = tableData[i].isbn;
-            cell2.innerHTML = tableData[i].title;
-            cell3.innerHTML = tableData[i].author;
+                counter +=1;
+                var row = table.insertRow(counter);
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                var cell3 = row.insertCell(2);
+                var cell4 = row.insertCell(3);
+                var cell5 = row.insertCell(4);
+                var cell6 = row.insertCell(5);
+                // var ce11 1 is checkbox
+                cell1.innerHTML = "<input type='checkbox' id='checkbox'>";
+                cell4.innerHTML = tableData[i].isbn;
+                cell2.innerHTML = tableData[i].title;
+                cell3.innerHTML = tableData[i].author;
+                cell5.innerHTML = tableData[i].date;
+                cell6.innerHTML = tableData[i].returnDate;
         }
         }
         xhr.onreadystatechange = null;
